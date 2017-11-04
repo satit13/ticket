fa93b0d0b9ff061155a4a728815841fde5d509d0
satit <satit@nopadol.com>
2017-11-04 09:26:28 +0700

    version 1.0 - Cut off

--- script-update-database/5.script-v_ticket_sale.sql
+++ script-update-database/5.script-v_ticket_sale.sql
@@ -1,17 +1,21 @@
 USE [td]
 GO
 
-/****** Object:  View [dbo].[v_ticket_sale]    Script Date: 10/02/2017 14:03:30 ******/
+/****** Object:  View [dbo].[v_ticket_sale]    Script Date: 10/19/2017 15:31:36 ******/
 SET ANSI_NULLS ON
 GO
 
 SET QUOTED_IDENTIFIER ON
 GO
+--select * from v_ticket_sale where TICKET='TK2679'
 
-create view [dbo].[v_ticket_sale]
+ALTER view [dbo].[v_ticket_sale]
 as 
-select a1.*,
-	a2.* ,inv.* 
+select a1.* ,
+	(select top 1 substring(itemname,1 ,254)  as itemname
+			from bcsaleordersub a left join BCSaleOrder b on a.docno = b.DocNo   
+			where a.DocNo = a2.so_docno and itemcode not like '%.%') as First_item,
+	a2.* ,inv.*
 	from 
 (
 	select  a.Code AS TICKET,a.Name AS TICKET_NAME,
@@ -38,21 +42,24 @@ select docno as so_docno,
 ) a2 on a1.TICKET=a2.so_projectcode
 left join 
 (
-select docno as inv_docno,
-	docdate as inv_docdate,
-	projectcode as inv_projectcode,
-	totalamount as inv_totalamount,
-	case when billtype in (0,2) then 'เงินสด' 
-		when billtype in (1,3) then 'เงินเชื่อ'
+select a.docno as inv_docno,
+	a.docdate as inv_docdate,
+	b.projectcode as inv_projectcode,
+	sum(amount) as inv_totalamount,
+	case when b.billtype in (0,2) then 'เงินสด' 
+		when b.billtype in (1,3) then 'เงินเชื่อ'
 		end as inv_billtype,
-	case when billtype in (0,2) then 0 else billbalance end as inv_billbalance  ,
-	arcode ,
+	case when b.billtype in (0,2) then 0 else sum(b.billbalance) end as inv_billbalance  ,
+	a.arcode ,
 	case when BillType in (0,2) then 'เก็บเงินแล้ว' 
-		when BillType in (1,3) and BillBalance =0 then 'เงินเชื่อ-เก็บเงินแล้ว'
-		when BillType in (1,3) and BillBalance > 0 then 'ค้างชำระ'
-		end as billstatus
-	from BCARINVOICE where IsCancel=0			
-)inv on a1.TICKET = inv_ProjectCode
+		when BillType in (1,3) and sum(b.billbalance) =0 then 'เงินเชื่อ-เก็บเงินแล้ว'
+		when BillType in (1,3) and sum(b.billbalance) > 0 then 'ค้างชำระ'
+		end as billstatus,
+		a.SORefNo
+	from BCARINVOICESUB a left join BCARINVOICE b on a.DocNo = b.DocNo  where b.IsCancel=0	
+			group by a.DocNo,a.DocDate,b.BillType,a.SORefNo,b.ProjectCode,a.arcode
+)inv on a1.TICKET = inv_ProjectCode and a2.so_docno = inv.SORefNo
+
 
 
 
